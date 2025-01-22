5e075c28c34fd5925ccbd638ef30687fec31867e
Sundhararajan Vinodh Sangeetha <sundharvs@gmail.com>
2025-01-22 00:45:21 -0500

    remove build artifacts

--- cmake_commands.sh
+++
@@ -1,3 +0,0 @@
-/usr/bin/cmake /home/sundharvs/ros2_ws/src/slam_path -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -G Ninja -DCMAKE_INSTALL_PREFIX=/home/sundharvs/ros2_ws/install/slam_path
-/usr/bin/cmake --build /home/sundharvs/ros2_ws/build/slam_path -- -j8 -l8
-/usr/bin/cmake --install /home/sundharvs/ros2_ws/build/slam_path
